8b1ec3ff864837aef73d7d84e86e2f651b5ca98e
Gon <gon27.tk08@gmail.com>
2020-02-02 01:13:00 +0900

    塗り潰しの四角形から画像へ変更

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 addEventListener("load", main);
 addEventListener("mousemove", mouseMove);
+addEventListener('click', mouseClick);
 
 let canvas;     //キャンバス
 let context;    //コンテキスト
@@ -20,7 +21,22 @@ function createCanvas(_width, _height) {
     //console.log(context);
 }
 
-let currentdiff = 2;    // 現在の難易度
+function mouseMove(e) {
+    let rect = canvas.getBoundingClientRect();
+    Mouse.x = e.clientX - rect.left;
+    Mouse.y = e.clientY - rect.top;
+}
+
+function mouseClick(e) {
+    // console.log("clicked");
+    // console.log(" uoooo ;x = " + Mouse.x + ": y = " + Mouse.y);
+}
+
+let currentdiff = 0;    // 現在の難易度
+
+let array;  // 爆弾等を記録する配列
+let caver_array;    // 外側の側を記録する配列
+let images = new Array(12);
 
 const windowX = [8, 16, 24];  // フレームの幅
 const windowY = [8, 16, 24];  // フレームの高さ
@@ -36,146 +52,7 @@ const BOM = 10;
 let width;
 let height;
 
-let array;
-
-function init(){
-    // 配列の初期化
-    let H = windowY[currentdiff];
-    let W = windowX[currentdiff];
-    array = new Array(H);
-    for(let y = 0; y < H; y++){
-        array[y] = new Array(W);
-        for(let x = 0; x < W; x++){
-            array[y][x] = 0;
-            // console.log(array[y][x]);
-            // console.log(1);
-        }
-    }
-
-    // 爆弾の配置
-    let bomCount = 0;
-    while(bomCount != boms[currentdiff]){
-        let bx = getRandomRange(0, W);
-        let by = getRandomRange(0, H);
-        if (array[by][bx] == 0){
-            array[by][bx] = 10;
-            bomCount++;
-        }
-        // console.log(bomCount);
-    }
-
-    
-    // 爆弾の周りに数字を描画
-    for (let y = 0; y < H; y++) {
-        for (let x = 0; x < W; x++) {
-            if(array[y][x] !== 10) continue;
-
-            countRangeBom(x, y, W, H);
-        }
-    }
-
-    for (let y = 0; y < H; y++) {
-        console.log(array[y]);
-    }
-}
-
-// 爆弾のある範囲をカウントする
-// TODO: 爆弾を巻き込まないようにカウントする
-function countRangeBom(_x, _y, _W, _H){
-    console.log("y = " + _y + ", x = " + _x + ", val = " + array[_y][_x]);
-    for(let i = 0; i < 8; i++){
-        let iy = _y + dy[i];
-        let ix = _x + dx[i];
-        if(!outOfArray(ix, iy, _W, _H) && array[iy][ix] != 10){
-            console.log(i + ": iy = " + iy + ", ix = " + ix + ", array = " + array[iy][ix]);
-
-            array[iy][ix]++;
-        }
-    }
-}
-
-function outOfArray(_x, _y, _W, _H){
-    if (_x < 0 || _x > _W - 1 || _y < 0 || _y > _H - 1) {
-        return true;
-    }
-
-    return false;
-}
-
-function draw(){
-    // マス目描画
-    let H = windowY[currentdiff];
-    let W = windowX[currentdiff];
-    for (let y = 0; y < H; y++) {
-        for (let x = 0; x < W; x++) {
-            // if(array[y][x] === 0){
-            //     // console.log(11);
-            //     context.fillStyle = "rgb(255, 255, 0)";
-            //     drawRect(x * size, y * size, size);
-            // }else if(array[y][x] == 10){
-            //     context.fillStyle = "rgb(255, 0, 0)";
-            //     drawRect(x * size, y * size, size);
-            // }
-
-            drawTest(x, y, array[y][x]);
-        }
-    }
-}
-
-function drawTest(_x, _y, _num){
-    switch(_num){
-        case 10: {
-            context.fillStyle = "rgb(255, 255, 255)";
-            drawRect(_x * size, _y * size, size);
-            break;
-        }
-
-        case 0:{
-            context.fillStyle = "rgb(0, 255, 0)";
-            drawRect(_x * size, _y * size, size);
-            break;
-        }
-
-        case 1: {
-            context.fillStyle = "rgb(255, 0, 255)";
-            drawRect(_x * size, _y * size, size);
-            break;
-        }
-
-        case 2: {
-            context.fillStyle = "rgb(255, 0, 0)";
-            drawRect(_x * size, _y * size, size);
-            break;
-        }
-
-        case 3: {
-            context.fillStyle = "rgb(0, 0, 255)";
-            drawRect(_x * size, _y * size, size);
-            break;
-        }
-
-        case 4: {
-            context.fillStyle = "rgb(0, 0, 255)";
-            drawRect(_x * size, _y * size, size);
-            break;
-        }
-
-        case 5: {
-
-            break;
-        }
-
-        case 6: {
-
-            break;
-        }
-    }
-}
-
-function drawRect(_x, _y, _size){
-    context.fillRect(_x, _y, _size, _size);
-    context.strokeRect(_x, _y, _size, _size);
-}
+let image;
 
 function main(){
     if(currentdiff < 0 || currentdiff > 2){
@@ -188,30 +65,189 @@ function main(){
 
     createCanvas(width, height);
 
-    init();
-    draw();
+    loadImages();
 
-    // setInterval(update, 1000 / 60);
+    init();
+    // draw();
+
+    setInterval(update, 1000 / 60);
 }
 
-function update(){
+function init(){
+    let H = windowY[currentdiff];
+    let W = windowX[currentdiff];
+
+    initArray(W, H);
+    setBom(W, H);
+    calcBomRange(W, H);
+    // debugArray(H);
+}
+
+function update() {
     getMousePosition();
     console.log("x = " + Mouse.x + ": y = " + Mouse.y);
+    // context.drawImage(images[0], 0, 0);
+    // context.drawImage(images[1], size, 0);
+    // context.drawImage(images[2], size * 2, 0);
+    // context.drawImage(images[3], size * 3, 0);
+    // context.drawImage(images[4], 0, size);
+    // context.drawImage(images[5], 0, size * 2);
+    // context.drawImage(images[6], 0, size * 3);
+    // context.drawImage(images[7], size, size);
+    // context.drawImage(images[8], size * 2, size * 2);
+    // context.drawImage(images[9], size * 3, size * 3);
 
+    draw();
 }
+
+function initArray(_W, _H){
+    array = new Array(_H);
+    for (let y = 0; y < _H; y++) {
+        array[y] = new Array(_W);
+        for (let x = 0; x < _W; x++) {
+            array[y][x] = 0;
+            // console.log(array[y][x]);
+            // console.log(1);
+        }
+    }
+}
+
+// 爆弾の配置
+function setBom(_W, _H){
+    let bomCount = 0;
+    while (bomCount != boms[currentdiff]) {
+        let bx = getRandomRange(0, _W);
+        let by = getRandomRange(0, _H);
+        if (array[by][bx] == 0) {
+            array[by][bx] = 9;
+            bomCount++;
+        }
+    }
+}
+
+// 爆弾のある範囲をカウントする
+function countRangeBom(_x, _y, _W, _H){
+    console.log("y = " + _y + ", x = " + _x + ", val = " + array[_y][_x]);
+    for(let i = 0; i < 8; i++){
+        let iy = _y + dy[i];
+        let ix = _x + dx[i];
+        if(!outOfArray(ix, iy, _W, _H) && array[iy][ix] != 9){
+            console.log(i + ": iy = " + iy + ", ix = " + ix + ", array = " + array[iy][ix]);
+
+            array[iy][ix]++;
+        }
+    }
+}
+
+function calcBomRange(_W, _H){
+    // 爆弾の周りに数字を描画
+    for (let y = 0; y < _H; y++) {
+        for (let x = 0; x < _W; x++) {
+            if (array[y][x] !== 9) continue;
+            countRangeBom(x, y, _W, _H);
+        }
+    }
+}
+
+function outOfArray(_x, _y, _W, _H){
+    if (_x < 0 || _x > _W - 1 || _y < 0 || _y > _H - 1) {
+        return true;
+    }
+
+    return false;
+}
+
+function loadImages(){
+    for (let i = 0; i < 12; i++) {
+        images[i] = new Image();
+    }
+    images[0].src = "Aseet/none.png";
+
+    images[1].src = "Aseet/one.png";
+    images[2].src = "Aseet/twe.png";
+    images[3].src = "Aseet/three.png";
+    images[4].src = "Aseet/four.png";
+    images[5].src = "Aseet/five.png";
+    images[6].src = "Aseet/six.png";
+    images[7].src = "Aseet/seven.png";
+    images[8].src = "Aseet/eight.png";
+
+    images[9].src = "Aseet/bom.png";
+
+    images[10].src = "Aseet/flag.png";
+    images[11].src = "Aseet/wall.png";
+}
+
+function draw(){
+    // マス目描画
+    let H = windowY[currentdiff];
+    let W = windowX[currentdiff];
+    for (let y = 0; y < H; y++) {
+        for (let x = 0; x < W; x++) {
+            // drawTest(x, y, array[y][x]);
+            context.drawImage(images[array[y][x]], x * size, y * size);
+        }
+    }
+}
+
+// function drawTest(_x, _y, _num){
+//     switch(_num){
+//         case 10: {
+//             context.fillStyle = "rgb(255, 255, 255)";
+//             drawRect(_x * size, _y * size, size);
+//             break;
+//         }
+
+//         case 0:{
+//             context.fillStyle = "rgb(0, 255, 0)";
+//             drawRect(_x * size, _y * size, size);
+//             break;
+//         }
+
+//         case 1: {
+//             context.fillStyle = "rgb(255, 0, 255)";
+//             drawRect(_x * size, _y * size, size);
+//             break;
+//         }
+
+//         case 2: {
+//             context.fillStyle = "rgb(255, 0, 0)";
+//             drawRect(_x * size, _y * size, size);
+//             break;
+//         }
+
+//         case 3: {
+//             context.fillStyle = "rgb(0, 0, 255)";
+//             drawRect(_x * size, _y * size, size);
+//             break;
+//         }
+
+//         case 4: {
+//             context.fillStyle = "rgb(0, 0, 255)";
+//             drawRect(_x * size, _y * size, size);
+//             break;
+//         }
+//     }
+// }
+
+// function drawRect(_x, _y, _size){
+//     context.fillRect(_x, _y, _size, _size);
+//     context.strokeRect(_x, _y, _size, _size);
+// }
 
 function getMousePosition(){
     Mouse.x = Math.floor(Mouse.x / size);
     Mouse.y = Math.floor(Mouse.y / size);
 }
 
-function mouseMove(e){
-    let rect = canvas.getBoundingClientRect();
-    Mouse.x = e.clientX - rect.left;
-    Mouse.y = e.clientY - rect.top;
-}
 
 // [min, max)の範囲のランダムな数値を取得
 function getRandomRange(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+function debugArray(_H){
+    for (let y = 0; y < _H; y++) {
+        console.log(array[y]);
+    }
 }
